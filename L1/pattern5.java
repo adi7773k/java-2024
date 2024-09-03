7c65d9aa342d79ec2e31d93330b6bab56202c2c5
Aditya <adi7773k@gmail.com>
2024-09-03 13:09:43 +0530

    pattern q1

---
+++ L1/pattern5.java
@@ -0,0 +1,24 @@
+public class pattern5 {
+    public static void main(String[] args) {
+        // int n = 5;
+        // for (int i=n;i>=1;i--) 
+        // {    
+        //     for(int k=1; k<=n-i;k++ )
+        //     System.out.print(" ");   
+        //     for (int j=1;j<=i;j++) 
+        //     System.out.print("*");
+        // System.out.println();
+        // }
+        int n=5;
+        for(int i=n;i>=1;i--)
+        {
+                    for(int k=1; k<=n-i;k++ )
+                        System.out.print(" ");
+                    for(int j=1;j<=i;j++)
+                        System.out.print("*");
+                    System.out.println("");                          
+            
+        }
+        
+    }
+}
