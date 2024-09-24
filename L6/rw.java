3921941b8468fda17079577e0df67f9837f1217d
Aditya <adi7773k@gmail.com>
2024-09-24 10:10:13 +0530

    bs searchspace

--- L6/rw.java
+++ L6/rw.java
@@ -2,8 +2,5 @@ public class rw {
     public static void main(String[] args) {
         int j=11%8;
         System.out.println(j);
-        for (String string : args) {
-            
-        }
     }
 }
